5e1589df0db22d3113834701c0142e9dd5faf204
Raman Tehlan <ramantehlan@gmail.com>
2020-06-17 02:14:06 +0530

    configure typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,18 @@
 module.exports = {
-  stories: ['../src/**/*.stories.[tj]s'],
+  stories: ['../stories/**/*.stories.(ts|tsx)'],
+  webpackFinal: async config => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('ts-loader'),
+      },
+      {
+        loader: require.resolve('react-docgen-typescript-loader'),
+      },
+    ],
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+  return config;
+},
 };
---
+++ stories/index.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default {
+  title: 'Button',
+  parameters: {
+      info: { inline: true },
+    },
+};
+
+export const withEmoji = () => (
+    <div>
+          <span role="img" aria-label="so cool">
+                  😀 😎 👍 💯
+                </span>
+                  </div>
+);
