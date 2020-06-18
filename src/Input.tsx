22d3b7b8be605c654e167dbe8875e6ea7f4e9d83
Raman Tehlan <ramantehlan@gmail.com>
2020-06-19 04:45:18 +0530

    Add Input component

---
+++ src/Input.tsx
@@ -0,0 +1,54 @@
+import React, { FC,  HTMLAttributes} from "react";
+import styled from "styled-components";
+
+const Box = styled.div<Props>`
+ color:  #545454;
+ font-family: roboto;
+ font-size: 14px;
+
+input, select {
+  height: 30px; 
+  min-width: 80px;
+  background-color: #fff;
+  border: 1px solid #EBD8D8;
+  border-radius: 2px;
+  padding-left: 5px;
+}
+
+label {
+  position: absolute;
+  margin-top: -25px;
+}
+
+`;
+
+
+
+
+export interface Props extends HTMLAttributes<HTMLInputElement | HTMLSelectElement> {
+  children?: React.ReactNode | string;
+  variant?: "select" | "input";
+  label?: string;
+}
+
+export const Input: FC<Props> = (props: Props) => {
+      return (
+        <Box >
+          <label >{props.label}</label>
+          { props.variant == "input" 
+              ? <input  id={props.id} type="input" {...props} />
+              : <select id={props.id} {...props} >
+                  {props.children}
+              </select>
+          }
+        </Box>
+      );
+  } 
+
+Input.defaultProps = {
+  id: "name",
+  variant: "input",
+  label: "label",
+}
+
+
---
+++ stories/input.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Input } from '../src/Input';
+
+export default {
+  title: 'Input',
+  decorators: [ (storyFn: () => {}) => <div style={{marginTop: '50px'}}>{storyFn()}</div>],
+  parameters: {
+      info: {
+        inline: false,
+      },
+    },
+};
+
+
+export const basic = () => (
+  <Input />
+);
+
+export const select = () => (
+  <Input variant="select" >
+      <option> Value 1 </option>
+    <option> Value 2 </option>
+  </Input>
+);
+
+
