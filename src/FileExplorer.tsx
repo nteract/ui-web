97c5d946fc384bc72064574a735cae8ab805f67c
Raman Tehlan <ramantehlan@gmail.com>
2020-06-23 09:44:04 +0530

    Add sidebar from ui-refresh

---
+++ src/FileExplorer.tsx
@@ -0,0 +1,33 @@
+import React, { FC,  HTMLAttributes} from "react";
+import styled from "styled-components";
+
+
+const ConsoleDiv = styled.div<Props>`
+  clear: left;
+  min-height: 200px;
+  padding: 20px;
+  color: #f1f1f1;
+  font-family: Monaco, monospace;
+  font-size: 12px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  background-color: #22223B;
+  overflow: auto;
+  counter-reset: line-numbering;
+  border-top: 1px solid #000;
+  z-index: 1000;
+`;
+
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+}
+
+export const Console: FC<Props> = (props: Props) => {
+      return (
+        <ConsoleDiv {...props} >
+          {props.children} 
+        </ConsoleDiv>
+      );
+  } 
+
---
+++ stories/explorer.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Sidebar } from '../src/Sidebar';
+
+export default {
+  title: 'Explorer',
+  parameters: {
+      info: {
+        inline: false,
+      },
+    },
+};
+
+const directoryList = [
+  {
+    name: 'My Root Folder',
+    subItems: [
+      { name: 'My Subfolder', subItems: [{ name: 'lasers.ipynb' }] },
+      { name: 'notebook.ipynb' },
+      { name: 'test.ipynb' },
+      {
+        name: 'Another Folder',
+        subItems: [{ name: 'recentTrends.ipynb' }, { name: 'newData.ipynb' }],
+      },
+    ],
+  },
+];
+
+
+export const basic = () => (
+  <Sidebar subItems={directoryList}> </Sidebar>
+);
